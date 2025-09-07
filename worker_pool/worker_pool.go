785841b6284a3bae5edca5f094fb6a76fc4e5e22
Lirprocs <lirprocs@gmail.com>
2025-09-07 21:27:44 +0300

    Initial commit

---
+++ worker_pool/worker_pool.go
@@ -0,0 +1,94 @@
+package worker_pool
+
+import (
+	"sync"
+)
+
+type WorkerPool struct {
+	tasks    chan func()
+	wg       sync.WaitGroup
+	stopOnce sync.Once
+	stopCh   chan struct{}
+	stopped  bool
+	mu       sync.Mutex
+}
+
+func NewWorkerPool(numberOfWorkers int) *WorkerPool {
+	wp := &WorkerPool{
+		tasks:  make(chan func(), 1024),
+		stopCh: make(chan struct{}),
+	}
+
+	for i := 0; i < numberOfWorkers; i++ {
+		go wp.worker()
+	}
+
+	return wp
+}
+
+func (wp *WorkerPool) worker() {
+	for {
+		select {
+		case <-wp.stopCh:
+			return
+		case task, ok := <-wp.tasks:
+			if !ok {
+				return
+			}
+			task()
+			wp.wg.Done()
+		}
+	}
+}
+
+// Submit - добавить таску в воркер пул
+func (wp *WorkerPool) Submit(task func()) {
+	wp.mu.Lock()
+	if wp.stopped {
+		wp.mu.Unlock()
+		panic("Submit called after Stop/StopWait")
+	}
+	wp.mu.Unlock()
+
+	select {
+	case wp.tasks <- task:
+		wp.wg.Add(1)
+	case <-wp.stopCh:
+		panic("Submit called after Stop/StopWait")
+	}
+}
+
+// SubmitWait - добавить таску в воркер пул и дождаться окончания ее выполнения
+func (wp *WorkerPool) SubmitWait(task func()) {
+	done := make(chan struct{})
+	wp.Submit(func() {
+		task()
+		close(done)
+	})
+	<-done
+}
+
+// Stop - остановить воркер пул, дождаться выполнения только тех тасок, которые выполняются сейчас
+func (wp *WorkerPool) Stop() {
+	wp.stopOnce.Do(func() {
+		wp.mu.Lock()
+		wp.stopped = true
+		wp.mu.Unlock()
+
+		close(wp.stopCh)
+		close(wp.tasks)
+	})
+}
+
+// StopWait - остановить воркер пул, дождаться выполнения всех тасок, даже тех, что не начали выполняться, но лежат в очереди
+func (wp *WorkerPool) StopWait() {
+	wp.stopOnce.Do(func() {
+		wp.mu.Lock()
+		wp.stopped = true
+		wp.mu.Unlock()
+
+		close(wp.tasks)
+		wp.wg.Wait()
+		close(wp.stopCh)
+	})
+}
---
+++ worker_pool/worker_pool_test.go
@@ -0,0 +1,224 @@
+package worker_pool
+
+import (
+	"sync"
+	"sync/atomic"
+	"testing"
+	"time"
+)
+
+func TestSubmit(t *testing.T) {
+	var counter int32
+	numTask := 10
+
+	wp := NewWorkerPool(5)
+
+	for i := 0; i < numTask; i++ {
+		wp.Submit(func() {
+			atomic.AddInt32(&counter, 1)
+		})
+	}
+
+	wp.StopWait()
+	if counter != int32(numTask) {
+		t.Errorf("TestSubmit: tasks did not run")
+	}
+}
+
+func TestParallelExecution(t *testing.T) {
+	var counter int32
+	numTask := 10
+
+	wp := NewWorkerPool(10)
+	start := time.Now()
+	for i := 0; i < numTask; i++ {
+		wp.Submit(func() {
+			atomic.AddInt32(&counter, 1)
+			time.Sleep(1000 * time.Millisecond)
+		})
+	}
+	wp.StopWait()
+	stop := time.Since(start)
+
+	if stop > 2000*time.Millisecond {
+		t.Errorf("TestParallelExecution: tasks did not run in parallel, took %v", stop)
+	}
+}
+
+func TestSubmitAfterStop(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("TestSubmitAfterStop: expected panic when submitting after Stop, but got none")
+		}
+	}()
+
+	wp := NewWorkerPool(2)
+	wp.Stop()
+	wp.Submit(func() {})
+}
+
+func TestSubmitWait(t *testing.T) {
+	var counter int32
+	numTask := 3
+
+	wp := NewWorkerPool(2)
+	for i := 0; i < numTask; i++ {
+		wp.SubmitWait(func() {
+			atomic.AddInt32(&counter, 1)
+			time.Sleep(1000 * time.Millisecond)
+		})
+	}
+	wp.Stop()
+
+	if counter != int32(numTask) {
+		t.Errorf("TestSubmitWait: expected %d, got %d", numTask, counter)
+	}
+}
+
+func TestStop(t *testing.T) {
+	var counter int32
+	numTask := 50
+
+	wp := NewWorkerPool(2)
+
+	for i := 0; i < numTask; i++ {
+		wp.Submit(func() {
+			time.Sleep(100 * time.Millisecond)
+			atomic.AddInt32(&counter, 1)
+		})
+	}
+
+	wp.Stop()
+
+	if counter == int32(numTask) {
+		t.Errorf("TestStop: expected %d, got %d", numTask, counter)
+	}
+}
+
+func TestStopAfterStop(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			t.Errorf("TestStopAfterStop: expected no panic on second Stop, but got: %v", r)
+		}
+	}()
+
+	wp := NewWorkerPool(2)
+
+	wp.Submit(func() {})
+
+	wp.Stop()
+	wp.Stop()
+}
+
+func TestStopWait(t *testing.T) {
+	var counter int32
+	numTask := 5
+
+	wp := NewWorkerPool(2)
+
+	for i := 0; i < numTask; i++ {
+		wp.Submit(func() {
+			time.Sleep(20 * time.Millisecond)
+			atomic.AddInt32(&counter, 1)
+		})
+	}
+
+	wp.StopWait()
+
+	if counter != 5 {
+		t.Errorf("TestStopWait: expected %d, got %d", numTask, counter)
+	}
+}
+
+func TestStopWaitAfterStopWait(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			t.Errorf("TestStopWaitAfterStopWait: expected no panic on second Stop, but got: %v", r)
+		}
+	}()
+
+	wp := NewWorkerPool(2)
+
+	wp.Submit(func() {})
+
+	wp.StopWait()
+	wp.StopWait()
+}
+
+func TestStopWaitAfterStop(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			t.Errorf("TestStopWaitAfterStop: expected no panic on second Stop, but got: %v", r)
+		}
+	}()
+
+	wp := NewWorkerPool(2)
+
+	wp.Submit(func() {})
+
+	wp.Stop()
+	wp.StopWait()
+}
+
+func TestStopAfterStopWait(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			t.Errorf("TestStopAfterStopWait: expected no panic on second Stop, but got: %v", r)
+		}
+	}()
+
+	wp := NewWorkerPool(2)
+
+	wp.Submit(func() {})
+
+	wp.StopWait()
+	wp.Stop()
+}
+
+func TestConcurrentSubmitWait(t *testing.T) {
+	var counter int32
+	numTasks := 20
+	wp := NewWorkerPool(5)
+
+	var wg sync.WaitGroup
+	wg.Add(numTasks)
+
+	for i := 0; i < numTasks; i++ {
+		go func() {
+			defer wg.Done()
+			wp.SubmitWait(func() {
+				time.Sleep(50 * time.Millisecond)
+				atomic.AddInt32(&counter, 1)
+			})
+		}()
+	}
+
+	wg.Wait()
+	wp.Stop()
+
+	if counter != int32(numTasks) {
+		t.Errorf("TestConcurrentSubmitWait: expected %d, got %d", numTasks, counter)
+	}
+}
+
+func TestStopWithRemainingTasks(t *testing.T) {
+	var counter int32
+	numTasks := 10
+
+	wp := NewWorkerPool(2)
+
+	for i := 0; i < numTasks; i++ {
+		wp.Submit(func() {
+			time.Sleep(100 * time.Millisecond)
+			atomic.AddInt32(&counter, 1)
+		})
+	}
+
+	time.Sleep(150 * time.Millisecond)
+
+	wp.Stop()
+
+	if counter >= int32(numTasks) {
+		t.Errorf("TestStopWithRemainingTasks: expected some tasks to remain unexecuted, got %d/%d", counter, numTasks)
+	}
+}
